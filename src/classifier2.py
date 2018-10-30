5dda87693825ac4efca638bec5c84752671e10df
Alejandro Moreo Fernández <alejandro.moreo@isti.cnr.it>
2018-10-30 18:00:51 +0100

    big refactor

--- src/classifier2.py
+++ src/classifier2.py
@@ -1,46 +1,55 @@
 from sklearn.svm import *
 from sklearn.model_selection import cross_val_score, GridSearchCV
-from sklearn.feature_selection import SelectKBest, chi2
 from doc_representation2 import *
-import numpy as np
+from sklearn.metrics import f1_score, make_scorer
 
-nfolds = 2
-do_feat_selection = False
-params = [{'C': [0.0001, 0.001, 0.01, 0.1, 1, 10, 100, 1000],'class_weight':['balanced',None]}]
+probability=False
+# SVM = SVC
+SVM = LinearSVC
 
-path = 'Data'
-Xtr,ytr,ep2 = load_documents(path)
+nfolds = 3
+params = {'C': [0.0001, 0.001, 0.01, 0.1, 1, 10, 100, 1000], 'class_weight':['balanced',None]}
+if SVM is SVC:
+    params['kernel']=['linear','rbf']
 
-if do_feat_selection:
-    print('feature selection')
-    num_feats = int(0.1 * Xtr.shape[1])
-    feature_selector = SelectKBest(chi2, k=num_feats)
-    Xtr = feature_selector.fit_transform(Xtr,ytr)
-    print('final shape={}'.format(Xtr.shape))
-        #ep1 = feature_selector.transform(ep1)
-    ep2 = feature_selector.transform(ep2)
+path = '../testi'
 
+Xtr,ytr,ep1,ep2 = load_documents(path, split_documents=True, function_words_freq=True, tfidf=True, tfidf_feat_selection_ratio=0.1)
 
 # learn a SVM
-print('optimizing a SVM')
-svm_base = LinearSVC(max_iter=-1) #i risultati non convergevano, cosi' ho provato SVC piuttosto che LinearSVC
 
-svm_optimized = GridSearchCV(svm_base, param_grid=params, cv=nfolds)
-    #print ("Shape of X:", Xtr.shape)
-svm_optimized.fit(Xtr, ytr)
-    #print('Best params: {}'.format(svm_optimized.best_params_))
+# svm = SVM(probability=probability)
+svm = SVM()
+
+positive_examples = ytr.sum()
+if positive_examples>nfolds:
+    print('optimizing {}'.format(svm.__class__.__name__))
+    svm = GridSearchCV(svm, param_grid=params, cv=nfolds, scoring=make_scorer(f1_score))
+
+svm.fit(Xtr, ytr)
+
+if isinstance(svm, GridSearchCV):
+    print('Best params: {}'.format(svm.best_params_))
 
 # evaluation of results
 print('computing the cross-val score')
-accuracies = cross_val_score(svm_optimized, Xtr, ytr, cv=nfolds, n_jobs=-1)
-acc_mean, acc_std = accuracies.mean(), accuracies.std()
-print('Accuracy={:.3f} (+-{:.3f})'.format(acc_mean, acc_std))
+f1scores = cross_val_score(svm, Xtr, ytr, cv=nfolds, n_jobs=-1, scoring=make_scorer(f1_score))
+f1_mean, f1_std = f1scores.mean(), f1scores.std()
+print('F1-measure={:.3f} (+-{:.3f})\n'.format(f1_mean, f1_std))
 
 # final test
-print('predicting the Epistolas')
-    #ep1_ = svm_optimized.predict(ep1)
-ep2= np.reshape(ep2, (1,-1))
-ep2_ = svm_optimized.predict(ep2)
-    #print('Epistola1 acc = {:.3f} {}'.format(ep1_.mean(), ep1_))
-print('Epistola2 acc = {:.3f} {}'.format(ep2_.mean(), ep2_))
+def predictEpistola(ep, epistola_name):
+    pred = svm.predict(ep)
+    full_doc_prediction = pred[0]
+    print('{} is from Dante: {}'.format(epistola_name, 'Yes' if full_doc_prediction == 1 else 'No'))
+    if len(pred>0):
+        fragment_predictions= pred[1:]
+        print('fragments average {:.3f}, array={}'.format(fragment_predictions.mean(), fragment_predictions))
+        if SVM is SVC and probability:
+            prob = svm.predict_proba(ep)[:,1]
+            np.set_printoptions(precision=2, linewidth=200)
+            print('probabilistic view: full={:.3f}, fragments average {:.3f}, fragments={}'.format(prob[0], prob[1:].mean(), prob[1:]))
 
+print('Predicting the Epistolas')
+predictEpistola(ep1, 'Epistola 1')
+predictEpistola(ep2, 'Epistola 2')
